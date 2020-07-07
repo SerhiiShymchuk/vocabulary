b290a1c5690db76186948d1ffc426d5c3eb3f183
SerhiiShymchuk <shymaser@gmail.com>
2020-07-07 08:21:45 +0300

    startTraining, stepTraining, trainWord functions

--- train.js
+++ train.js
@@ -1,19 +1,56 @@
 const words = JSON.parse(localStorage.words || '[]')
+const wordsToTrain = []
 
 updMaxWords()
+
+startTraining(words.length/2)
 
 function updMaxWords() {
     numberInp.max = numberInp.value = words.length
 }
 
-function startTraining(num, times) {
+function stepTraining() {
+    if (wordsToTrain.length > 0) {
+        trainWord(wordsToTrain.pop())
+    } else {
+        checkBtn.disabled = true
+        
+    }
+}
+
+function trainWord(word) {
+    showInp.value = word.ukr
+    typeInp.value = ''
+    typeInp.focus()
+    checkBtn.onclick = function() {
+        if (typeInp.value == word.eng) {
+            typeInp.style.border = '1px solid green'
+            setTimeout(function () {
+                typeInp.style.border = null
+                stepTraining()
+            }, 700)
+            word.hit++
+        } else {
+            word.miss++
+            typeInp.style.border = '1px solid red'
+            setTimeout(function () {
+                typeInp.style.border = null
+                typeInp.value = word.eng
+                setTimeout(stepTraining, 3000)
+            }, 700)           
+        }
+        localStorage.words = JSON.stringify(words)
+    }
+}
+
+function startTraining(num, times=1) {
     const selectedWords = selectWordsToTrain(num)
-    const wordsToTrain = []
+    wordsToTrain.splice(0, wordsToTrain.length)
     for (let i = 0; i<times; i++) {
         wordsToTrain.push(...selectedWords)
     }
     shuffle(wordsToTrain)
-    return wordsToTrain
+    stepTraining()
 }
 
 function shuffle(arr) {
